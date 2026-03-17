62a3e9e2f093a17d301eaac6d74483ff6e1ad2d3
MrArjuna <128251686+MrArjuna@users.noreply.github.com>
2026-03-17 13:40:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ const vencimientosOriginales = [
   { empresa: "Tatuus 5 Bancos", fecha: "2026-05-01T12:00:00", nota: "" },
   { empresa: "Tatuus 5 Nominas", fecha: "2027-01-04T12:00:00", nota: "" },
  //{ empresa: "Tatuus 5 TS PLUS", fecha: "2026-02-12T12:00:00", nota: "" },
+  { empresa: "Zeitten TS PLUS", fecha: "2027-03-10T12:00:00", nota: "" },
   { empresa: "Tatuus 5 RSDG", fecha: "2025-09-05T09:00:00", nota: ""},
  // { empresa: "Zeitten RSDG", fecha: "2025-08-13T19:00:00", nota: "" },
 //{ empresa: "Server 1 Tatuus RSDG", fecha: "2025-10-20T19:00:00", nota: "" },
